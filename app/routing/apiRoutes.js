67f5aa73e118d656d99fad06eb9a0c0926621deb
Stephane Genini <sgenini@gmail.com>
2018-12-20 12:57:12 -0800

    Fixed variable confusion preventing best friend from appearing

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,20 +8,27 @@ var apiRoutes = function(app){
     app.post("/api/friends", function(req, res){
         var bestFriend = {
             name: "",
-            score: Infinity
+            friendDifference: Infinity
         }
+
+        var userData = req.body;
+        var userScores = userData.scores;
+
+        var scoreDifference;
 
         for (var i = 0; i < friends.length; i++){
             var friend = friends[i];
-            var scoreDifference = 0;
+            scoreDifference = 0;
+
+            console.log(friend.name);
 
             for (var j = 0; j < friend.scores.length; j++){
                 scoreDifference += Math.abs(parseInt(req.body.scores[j]) - parseInt(friend.scores[j]));
             }
 
-            if (scoreDifference <= bestFriend.score) {
+            if (scoreDifference <= bestFriend.friendDifference) {
                 bestFriend.name = friend.name;
-                bestFriend.score = friend.scoreDifference;
+                bestFriend.friendDifference = scoreDifference;
             }
         }
 
